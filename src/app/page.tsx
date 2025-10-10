f396828e010d058df1af9e1d84330193cce97d93
ImBSaga <imbsaga@gmail.com>
2025-10-10 20:18:27 +0700

    feat/Forced Products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { redirect } from "next/navigation";
 import { useAuth } from "@/providers/AuthProvider";
 
 export default function Home() {
   const { user } = useAuth();
+
+  // Go to products page
+  redirect("/products");
 
   return (
     <main className="p-6">
